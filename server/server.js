7827efd3048e14eca17a65d71c6f346da1e86ca1
Pham Nhu Y <ypn@VIJA.local>
2018-01-08 16:51:20 +0700

    ok gogo

--- server/server.js
+++ server/server.js
@@ -32,15 +32,102 @@ db.connect(function(err){
 });
 
 io.on('connection', function (socket) {
-  console.log('has new connection');
-  socket.on('will_connect', function (data) {
-    console.log('new device connected');
-    io.emit('new_device_connected',{position:data});
-  });
+  //new car tracking session
+  socket.on('new_car_tracking_detected', function (data) {
+    //get all checkponts
+    db.query("SELECT id FROM CHECKPOINTS",function(err,result){
+      if(err){
+        //Log lỗi
+        console.log(err)
+      }else{
+        var checkpoints =[]
+        for(i = 0; i< result.length ; i++){
+          var cp = {
+            checkpointId: result[i].id,
+            status:0,
+            time_start:'',
+            time_end:'',
+            total_time:''
+          }
+
+          checkpoints.push(cp);
+        }
+
+        var status = JSON.stringify(checkpoints);
+
+        var sql = `INSERT INTO POSITIONS_TRACKING (bienso,type,car_positions,status) VALUES('${data.bienso}',1,'[]','${status}')`;
+        db.query(sql, function (err, result) {
+            if (err) {//log lỗi
+              console.log(err);
+            }else{
+                socket.emit('create_tracking_session',{
+                  status:201,
+                  id:`${result.insertId}`
+                });
+
+                var ss = `SELECT id,bienso,status FROM POSITIONS_TRACKING WHERE id = ${result.insertId}`;
+
+                db.query(ss,function(err,result){
+                  if(err){
+                    console.log(err);
+                  }else{
+                    io.emit('new_session_detected',{data:JSON.stringify(result[0])});
+                  }
+                });
+
+            }
+        });
+      }
+    });//get all checkpoints
+  });//new car tracking session
 
   socket.on('update_position',function(data){
-    console.log('update_position');
-    io.emit('update_position',{marker:data});
+
+    var query = `UPDATE POSITIONS_TRACKING SET car_positions = JSON_ARRAY_APPEND(car_positions,'$','{lat:${data.lat},lng:${data.lng}}') WHERE ID = ${data.sessionId}` ;
+    var latlng = {
+      lat:data.lat,
+      lng:data.lng
+    };
+
+    db.query(query,function(err,result){
+      if(err){
+        console.log(err);
+      }
+    });
+
+    io.emit('update_position',{marker:latlng});
+  });
+
+  socket.on('session_step_into_checkpoint',function(data){
+    //Cập nhật lại trạng thái của sesssion.
+    var time_start = new Date();
+    var query = `UPDATE POSITIONS_TRACKING SET status = JSON_REPLACE(status,'$[${data.checkpointIndex}].status',1,'$[${data.checkpointIndex}].time_start','${time_start}') WHERE id = ${data.sessionId}`;
+
+    db.query(query,function(err,result){
+      if(err){
+        console.log(err);
+      }else{
+        io.emit('session_step_into_checkpoint',{data:data});
+        console.log('update success');
+      }
+    });
+
+  });
+
+  socket.on('session_step_out_checkpoint',function(data){
+
+    var time_end = new Date();
+    var query = `UPDATE POSITIONS_TRACKING SET status = JSON_REPLACE(status,'$[${data.checkpointIndex}].status',2,'$[${data.checkpointIndex}].time_end','${time_end}','$[${data.checkpointIndex}].total_time','${data.total_time}') WHERE id = ${data.sessionId}`;
+
+    db.query(query,function(err,result){
+      if(err){
+        console.log(err);
+      }else{
+        io.emit('session_step_out_checkpoint',{data:data});
+        console.log('update state car out success');
+      }
+    });
+
   });
 
   socket.on('disconnect',function(){
